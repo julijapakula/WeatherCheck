c4e0e19771495597a97f2c90ffb9b44b34171814
julijapakula <julija.pakula@gmail.com>
2019-12-22 15:02:50 +0200

    API WeatherRequest

--- src/test/java/requesters/WeatherRequester.java
+++ src/test/java/requesters/WeatherRequester.java
@@ -1,4 +1,16 @@
 package requesters;
 
+import model.Response;
+import org.springframework.web.client.RestTemplate;
+
 public class WeatherRequester {
+   private final String PREFIX = "https://samples.openweathermap.org/data/2.5/weather?q=";
+   private final String POSTFIX = ",uk&appid=b6907d289e10d714a6e88b30761fae22";
+
+   public Response requestWeather(String city){
+    String url = PREFIX + city + POSTFIX;
+   // dependency spring - web -- объект RestTemplate (запросы, получение обработки, результаты)
+       RestTemplate restTemplate = new RestTemplate();
+       restTemplate.getForEntity()
+    }
 }
